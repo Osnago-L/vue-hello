ab383d96e3cc4f418bc1d4463bd5ce92f7c5d431
Osnago-L <osna.brera@gmail.com>
2022-01-13 15:44:35 +0100

    create repo/end project

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Boolean Careers!',
+      source: 'https://coursereport-production.imgix.net/uploads/school/logo/681/original/Pittogramma-RGB-1080-BLUE-circle.png?w=200&h=200'
+    }
+  })
